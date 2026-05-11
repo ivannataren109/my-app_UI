eba7d47b7898da5ec71b11c5f19a94d1d5048425
ivannataren109 <ivannataren109@gmail.com>
2026-05-10 23:28:03 -0400

    fixed Box Import

--- src/shared/Dashboard.tsx
+++ src/shared/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useMemo } from 'react';
-import { Box, Grid } from '@mui/material';
+import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
